a30db06ee34baebb4a2673d52363d0c7667d4a1e
Narasimha rao Golla <167541683+Narasimha-143@users.noreply.github.com>
2025-02-14 14:55:25 +0530

    Created Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.*;
+public class Prime{
+  boolean isPrime(int number){
+        for(int i = 2; i <= Math.sqrt(number); i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+  }
+  public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Prime p = new Prime();
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Is a number Prime: " + p.isPrime(num));
+  }
